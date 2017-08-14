a67675443cbac9cb2ee0f02efa53407d373b96b0
Łukasz Tomalczyk <lukasz.tomalczyk@onet.pl>
2017-08-14 18:03:31 +0200

    pierwszy dzien

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JobForFreeTime.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JobForFreeTime.Startup))]
+namespace JobForFreeTime
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
